4c50fb616b4696c9c3c5ecfbf115c27fa944a156
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-10-16 16:36:05 +0600

    Six ways to do Conditional Rendering: If, Ternary and AND OR

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -17,9 +17,46 @@
 
 
 //Conditional Rendering Option 2:
+// export default function Todo({task, isDone}){
+//        if(isDone){
+//         return <li>Finished : {task}</li>
+//        } 
+//        return <li>Work Continue: {task}</li> 
+//     }
+
+//Conditional Rendering Option 3: ternary operator
+
+// export default function Todo({task, isDone}){
+//     return(
+//         <li>{isDone ? 'Finish' : 'Work continue'}: {task}</li>
+//     )
+// }
+
+//Conditional Rendering Option 4: &&
+// export default function Todo({task, isDone}){
+//     return(
+//         <li>{task} {isDone && ': Hoye geche'}</li>
+//     )
+// }
+
+
+//Conditional Rendering Option 5: or "||"
+// export default function Todo({task, isDone}){
+//     return(
+//         <li>{task} {isDone || ': Do it'}</li>
+//     )
+// }
+
+
+//Conditional Rendering Option 6:
+
 export default function Todo({task, isDone}){
+    let lisItem;
        if(isDone){
         return <li>Finished : {task}</li>
        } 
-       return <li>Work Continue: {task}</li> 
+       else{
+         lisItem = <li>Work Continue: {task}</li>
+       } 
+       return lisItem;
     }
